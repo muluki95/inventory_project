790d8daee7e6a8343276053ff003ce7e29761e85
muluki95 <mulukiesther@gmail.com>
2024-01-07 15:43:20 -0600

    save

--- inventory_project/settings.py
+++ inventory_project/settings.py
@@ -156,14 +156,14 @@ LOGIN_REDIRECT_URL = 'dashboard_index'
 
 LOGIN_URL = 'user-login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_HOST = 'smtp.gmail.com'
 
-EMAIL_PORT = 587
+#EMAIL_PORT = 587
 
-EMAIL_USE_TLS = True
+#EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = ''
+#EMAIL_HOST_USER = ''
 
-EMAIL_HOST_PASSWORD = ''
+#EMAIL_HOST_PASSWORD = ''
